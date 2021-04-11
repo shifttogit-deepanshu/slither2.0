3ca77bbbec44da9c0f85c0bc410c36f51742f22b
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-04-12 03:25:14 +0530

    added basics

--- main.py
+++ main.py
@@ -4,4 +4,45 @@ screen = turtle.Screen()
 
 dh = turtle.Turtle()
 
+dh.shape("square")
+
+dh.penup()
+
+
+def moveUp():
+    dh.setheading(90)
+    dh.forward(2)
+
+def moveRight():
+    dh.setheading(0)
+    dh.forward(2)
+
+def moveLeft():
+    dh.setheading(180)
+    dh.forward(2)
+
+def moveDown():
+    dh.setheading(270)
+    dh.forward(2)
+
+screen.onkeypress(moveRight, "Right")
+
+
+screen.onkeypress(moveUp, "Up")
+
+
+screen.onkeypress(moveLeft, "Left")
+
+
+screen.onkeypress(moveDown, "Down")
+
+screen.listen()
+
+while True:
+    dh.forward(2)
+
+
+
+
+
 screen.exitonclick()
